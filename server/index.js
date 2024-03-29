553c61ca4305a63b4a2a83a5a02e40e6552e497b
Yaroslav Bryanskiy <yaroslav.bry@mail.ru>
2024-03-30 01:35:19 +0400

    fix typo

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
-const userRouter = require('./routes/item.routes')
+const itemRouter = require('./routes/item.routes')
 const cors = require('cors')
 const PORT = 3001
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use('/api', userRouter)
+app.use('/api', itemRouter)
 
 app.listen(PORT, () => console.log('server started on port ' + PORT))
+
