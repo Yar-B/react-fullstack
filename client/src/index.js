1bd935e6eb2cee3ef872d99da4c458577fb33f1b
Yaroslav Bryanskiy <yaroslav.bry@mail.ru>
2024-04-06 14:54:08 +0400

    added login form to client app

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
 import { HashRouter } from 'react-router-dom'
+import AppContextProvider from './AppContextProvider'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
 		<HashRouter>
-			<App />
+			<AppContextProvider />
 		</HashRouter>
 	</React.StrictMode>
 )
-
